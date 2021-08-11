ee90f47e4c887f97aead93ab85fafe13ffdf57c3
aagregory <16448213+aagregory@users.noreply.github.com>
2021-08-11 11:50:39 -0400

    initial commit

---
+++ install_nexp.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+
+#install and configure the node exporter #make sure you run as root.
+#add prometheus user if the user is already there
+#created by Greg Abbas
+
+#add the prometheus user
+system "useradd prometheus" or die $!;
+
+#unzip the package
+$thezip = 'node_exporter-1.1.2.linux-amd64.gz'
+
+    open unzip, "tar -xvf $thezip]" or die $1;
+        while $status = <unzip>{
+            print $status;
+        }   
+    close unzip;
+
+
+#move the files
+
+system "mv -rp node_exporter-1.1.2.linux-amd64 /usr/share/" or die $!; 
+system "chown -R prometheus.prometheus /usr/share/node_exporter-1.1.2.linux-amd64/" or die $!;
+system "cp node_exporter.service /etc/systemd/system/" or die $!;
+
+    open service, "servicectl enable node_exporter" or die $!;
+        while $service = <service>{
+            print $service;
+        }
+    close service;
+
+
+system "systemctl start node_exporter" or die $1;
+    
+    open state, "servicectl status node_exporter" or die $!;
+        while $state = <state>{
+        print $state;
+    }   close state;
+
+exit 0;
